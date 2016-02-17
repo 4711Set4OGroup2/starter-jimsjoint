987f55f76e50c252cce886c8345d944adf061b3a
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-16 17:12:15 -0800

    Fixed order display

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,6 @@ class Welcome extends Application {
         $this->data['pagebody'] = 'welcome';
 
         // Get all the completed orders
-        //FIXME
         $completed = $this->orders->some('status', 'c');
 
         // Build a multi-dimensional array for reporting
